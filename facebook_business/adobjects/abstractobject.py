a01293484db14876ba42c1ded72f60672a7d725f
Vitalii Polyvianyi <vitaliipolyvianyi@Vitaliis-MacBook-Pro.local>
2024-10-11 01:22:26 +0300

    down sleep for 0.05 sec

--- facebook_business/adobjects/abstractobject.py
+++ facebook_business/adobjects/abstractobject.py
@@ -141,10 +141,8 @@ class AbstractObject(collections_abc.MutableMapping):
             data = dict((k, self.export_value(v))
                         for k, v in data.items()
                         if v is not None)
-            time.sleep(0.5)
         elif isinstance(data, list):
             data = [self.export_value(v) for v in data]
-            time.sleep(0.5)
         return data
 
     def export_data(self):
@@ -154,7 +152,7 @@ class AbstractObject(collections_abc.MutableMapping):
         return self.export_all_data()
 
     def export_all_data(self):
-
+        time.sleep(0.05)
         return self.export_value(self._data)
 
     @classmethod
